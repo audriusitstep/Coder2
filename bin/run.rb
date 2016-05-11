c4813c386812919f928594146597280dd1938ca6
audriusitstep <audriusitstep@gmail.com>
2016-05-11 15:49:04 +0300

    coding text from file

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,6 @@ lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "coder"
 
-text = "aaaa bbbb"
+text = File.open('data/sample_text.txt', 'r').read
 coder = Coder::TextCoder.new(text, Coder::SimplePermutation.new)
 puts coder.get_coded_text
